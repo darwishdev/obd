015246dfd95c237762f5b9b62a5f183bd7f804df
ahmed darwish <99060595+darwishdev@users.noreply.github.com>
2023-05-06 03:33:10 +0300

    set email input label  to email or phone

--- lib/features/user/presentation/screens/login_screen.dart
+++ lib/features/user/presentation/screens/login_screen.dart
@@ -73,7 +73,7 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
                     return null;
                   },
                   prefixWidget: const Icon(Icons.email_outlined),
-                  labelText: "Email",
+                  labelText: "Email Or Phone",
                 ),
                 const SizedBox(height: 20),
                 FormFieldWidget(
